f31f32bb63cfc39353e65a493ad3191243b050f3
Arturo Gomez <arturorafa74@gmail.com>
2024-10-16 16:55:22 -0300

    seeder admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\User::create([
+            'name' => 'Admin', 
+            'email' => 'admin@buysmart.com', 
+            'ci' => '12345678',
+            'role' => 'admin',
+            'phone' => '12345678',
+            'birthdate' => '2000-01-01',
+            'password' => Hash::make('123456'),
+        ]);
+        
         $this->call([
             CategorySeeder::class,
             SubcategorySeeder::class,
