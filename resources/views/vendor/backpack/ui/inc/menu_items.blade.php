2fd4f6593815164d1204c9e73d05ee94e4ee6cb5
Arturo Gomez <arturorafa74@gmail.com>
2024-10-18 10:14:07 -0300

    .

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,6 +1,6 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<x-backpack::menu-dropdown title="Add-ons" icon="la la-puzzle-piece">
+<x-backpack::menu-dropdown title="Gestion de Usuarios" icon="la la-puzzle-piece">
     <x-backpack::menu-dropdown-header title="Authentication" />
     <x-backpack::menu-dropdown-item title="Users" icon="la la-user" :link="backpack_url('user')" />
     <x-backpack::menu-dropdown-item title="Roles" icon="la la-group" :link="backpack_url('role')" />
