fbb9c6b414c8a930b141885e4ec3fc2eb370d239
Arturo Gomez <arturorafa74@gmail.com>
2024-11-12 13:58:58 -0300

    seeder users test

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@buysmart.com',
+            'password' => Hash::make('123456'),
+            'role' => 'admin',
+            'active' => 1,
+            'ci' => '11111111',
+            'phone ' => '12345678',
+            'birthdate ' => '1990-01-01',
+        ]);
+
+        \App\Models\Customer::create([
+            'name' => 'Customer',
+            'email' => 'customer@buysmart.com',
+            'phone' => '12345678',
+            'ci' => '11111111',
+            'ruc' => '11111111-1',
+            'birthdate' => '1990-01-01',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
