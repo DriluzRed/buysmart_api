baf4e9f5289287227736132423fb758240eee640
Arturo Gomez <arturorafa74@gmail.com>
2024-11-20 00:48:31 -0300

    fix

--- database/seeders/ReportSeeder.php
+++ database/seeders/ReportSeeder.php
@@ -14,33 +14,6 @@ class ReportSeeder extends Seeder
     {
         \App\Models\Report::create([
             'name' => 'Reporte de ventas',
-            'custom_query' => 'SELECT 
-                orders.customer_id AS ClienteID,
-                COUNT(*) AS TotalPedidos,
-                SUM(subtotal) AS Subtotal,
-                SUM(additional_charges) AS CargosAdicionales,
-                SUM(total) AS TotalIngresos,
-                payment_method_id AS MetodoPago,
-                payment_status AS EstadoPago,
-                status AS EstadoPedido,
-                CONCAT(address_line_1, ' - ', address_line_2) AS direccion
-            FROM 
-                orders
-            JOIN payment_methods on orders.payment_method_id = payment_methods.id
-            LEFT JOIN addresses on addresses.id = orders.address_id
-            WHERE 
-                orders.deleted_at IS NULL
-                    [DATERANGE]
-            GROUP BY 
-                orders.customer_id, payment_method_id, payment_status, status, address_id
-            ORDER BY 
-                TotalIngresos DESC;
-            ',
-            'variables' => '{"DATERANGE": {"fields": {"orders": "created_at"}}}',
-        ]);
-
-        \App\Models\Report::create([
-            'name' => 'Reporte de ventas',
             'custom_query' => "SELECT 
                 orders.customer_id AS ClienteID,
                 COUNT(*) AS TotalPedidos,
@@ -61,8 +34,32 @@ class ReportSeeder extends Seeder
             GROUP BY 
                 orders.customer_id, payment_method_id, payment_status, status, address_id
             ORDER BY 
-                TotalIngresos DESC;",
+                TotalIngresos DESC;
+            ",
             'variables' => '{"DATERANGE": {"fields": {"orders": "created_at"}}}',
+        ]);
+
+        \App\Models\Report::create([
+            'name' => "Reporte de Productos mas vendidos',
+            'custom_query' => SELECT 
+                products.name AS Producto,
+                SUM(order_items.quantity) AS Cantidad_Vendida,
+                SUM(order_items.quantity * order_items.price) AS Total_Ingresos,
+                categories.name AS Categoria
+            FROM 
+                order_items
+            JOIN 
+                products ON order_items.product_id = products.id
+            LEFT JOIN 
+                categories ON products.category_id = categories.id
+            WHERE order_items.deleted_at IS NULL
+                    [DATERANGE]
+            GROUP BY 
+                products.id, products.name, categories.name
+            ORDER BY 
+                Cantidad_Vendida DESC;
+            ",
+            'variables' => '{"DATERANGE": {"fields": {"order_items": "created_at"}}}',
         ]);
 
         \App\Models\Report::create([
