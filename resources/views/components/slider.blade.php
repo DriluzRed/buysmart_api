8760beed496cbc71303b5a8bd65b02309f9b704f
Arturo Gomez <arturorafa74@gmail.com>
2024-12-07 11:23:08 -0300

    fix baner

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -2,8 +2,9 @@
     <div class="carousel-inner">
         @foreach ($banners as $key => $banner)
             <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-                <img src="{{ asset('storage/'.$banner->image) }}" class="d-block w-100" alt="Banner" style="height: 400px; object-fit: cover;">
-                <a href="{{ $banner->link }}" class="stretched-link"></a>
+                <div class="d-block w-100" style="height: 400px; background: url('{{ asset('storage/'.$banner['image']) }}') center center / cover no-repeat;">
+                    <a href="{{ $banner['link'] }}" class="stretched-link"></a>
+                </div>
             </div>
         @endforeach
     </div>
